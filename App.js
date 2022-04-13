976a6a1a40921c3e6b25e64119c63dd6bece9602
Julien <julienmulle@gmail.com>
2022-04-13 22:16:19 +0200

    tabBar navigation and stylised

--- App.js
+++ App.js
@@ -7,8 +7,11 @@
  */
 
 import React from 'react';
+import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+// ne pas oublier : npx react-native link react-native-vector-icons
+import Icon from 'react-native-vector-icons/Ionicons';
 
 //components
 import Menu from './src/screens/Menu';
@@ -20,15 +23,59 @@ const Tab = createBottomTabNavigator();
 
 const App = () => {
   return(
-   
+
       <NavigationContainer>
-        <Tab.Navigator screenOptions={{headerShown: false}}>
+        <Tab.Navigator 
+        screenOptions={({route}) => ({
+          tabBarIcon: ({color}) =>{
+            let iconName;
+            switch(route.name){
+              case 'Menu':
+                iconName = 'home';
+                break;
+                case 'Recipes':
+                iconName = 'restaurant';
+                break;
+                case 'ItemsList':
+                iconName = 'document';
+                break;
+                case 'ShoppingList':
+                iconName = 'cart';
+                break;
+                case 'search':
+                iconName =  'search';
+                break;
+            }
+            return <Icon  name={iconName} size={25}  color={color}/>
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'black',
+          tabBarShowLabel: false,
+          headerShown: false,
+          tabBarStyle: {
+            position: "absolute",
+            bottom: 15,
+            left: 15,
+            right: 15,
+            elevation: 0,
+            backgroundColor: "white",
+            borderRadius: 15,
+            height: 50,
+            // j'injecte le styles 
+            ...styles.shadow
+          }
+        })
+        }>
           <Tab.Screen 
           name="Menu"
           component={Menu}
           />
           <Tab.Screen 
           name="Recipes"
+          component={Recipes}
+          />
+          <Tab.Screen 
+          name="search"
           component={Recipes}
           />
           <Tab.Screen 
@@ -44,5 +91,21 @@ const App = () => {
    
   )
 }
-
+const styles = StyleSheet.create({
+  shadow:{
+    shadowColor: "#7F5DF0",
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.5,
+    elevation: 5
+  },
+  container: {
+    alignItems: 'flex-start',
+    //justifyContent: 'center', 
+    //top: 10
+  } 
+})
 export default App;
